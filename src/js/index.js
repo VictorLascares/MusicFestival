9be801ca552c9063d4f3c22f5f3daba4ee6fa905
Victor Lascares <vmlascares@gmail.com>
2021-05-26 01:06:14 -0500

    Agregando funcionalidad de mostrar imagen mas grande y agregando estilos a la galeria

--- src/js/index.js
+++ src/js/index.js
@@ -12,6 +12,9 @@ function createGallery() {
         for (let i = 1; i <= 12; i++) {
             const image = document.createElement('IMG');
             image.src = `build/img/thumb/${i}.webp`;
+            image.dataset.imageId = i;
+            // Funcion mostrar imagen
+            image.onclick = showImage;
             const list = document.createElement('LI');
             list.appendChild(image)
             galleryImages.appendChild(list);
@@ -20,9 +23,44 @@ function createGallery() {
         for (let i = 1; i <= 12; i++) {
             const image = document.createElement('IMG');
             image.src = `build/img/thumb/${i}.jpg`;
+            image.onclick = showImage;
             const list = document.createElement('LI');
             list.appendChild(image)
             galleryImages.appendChild(list);
         }
     } 
+}
+
+function showImage(e) {
+    const id = parseInt( e.target.dataset.imageId );
+    // Generar la imagen
+    const image = document.createElement('IMG');
+    image.src = `build/img/grande/${id}.webp`
+
+    const overlay = document.createElement('DIV');
+    overlay.appendChild(image);
+    overlay.classList.add('overlay');
+
+    overlay.onclick = function() {
+        overlay.remove();
+        body.classList.remove('fix-body');
+    }
+
+    // Boton para cerrar la imagen
+    const closeImage = document.createElement('P');
+    closeImage.textContent = 'X';
+    closeImage.classList.add('btn-close');
+
+    // cerrar imagen
+    closeImage.onclick = function() {
+        overlay.remove();
+        body.classList.remove('fix-body');
+    }
+
+    overlay.appendChild(closeImage);
+
+    // Mostrar en el HTML
+    const body = document.querySelector('body');
+    body.appendChild(overlay);
+    body.classList.add('fix-body')
 }
